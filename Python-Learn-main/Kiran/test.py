5bc7020e21d70d33d3bbc30990a41c8a3d7aa78b
shravankumar88 <shravankumar88@gmail.com>
2025-06-25 08:53:07 -0500

    new concepts

--- Python-Learn-main/Kiran/test.py
+++ Python-Learn-main/Kiran/test.py
@@ -1,12 +1,15 @@
-age = int(input("Enter the age for eligibility:" ))
-if age >= 59:
-    print(f"you are eligible for pension because of your age {age}")
-else:
-    print(f"your age is less for pension")
+# age = int(input("Enter the age for eligibility:" ))
+# if age >= 59:
+#     print(f"you are eligible for pension because of your age {age}")
+# else:
+#     print(f"your age is less for pension")
     
     
     
-    #51 For the given list add one element "hardik" and display the output
-cricketers = ["Dhoni", "kphli","Rohith"]
-cricketers_add = cricketers.insert["hardik"]
-print(cricketers_add)
+#     #51 For the given list add one element "hardik" and display the output
+# cricketers = ["Dhoni", "kphli","Rohith"]
+# cricketers_add = cricketers.append("hardik")
+# print(cricketers_add)
+
+if i <= 10:
+    print(i)
